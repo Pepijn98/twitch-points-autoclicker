101dff024cfb0e33204f16e491cd3a7a53cc9f6c
Pepijn van den Broek <root@kurozeropb.info>
2020-09-12 12:24:54 +0200

    Create index.meta.js

---
+++ index.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Twitch.tv Channel Points Auto-clicker
+// @namespace    twitch-points-autoclicker
+// @version      1.0.0
+// @description  Auto-click the green channel point button for you
+// @author       Pepijn98
+// @match        *://*.twitch.tv/*
+// @match        *://twitch.tv/*
+// @grant        none
+
+// @homepageURL https://github.com/Pepijn98/twitch-points-autoclicker
+// @supportURL https://github.com/Pepijn98/twitch-points-autoclicker/issues
+// @updateURL https://github.com/Pepijn98/twitch-points-autoclicker/master/index.meta.js
+// @downloadURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/master/index.user.js
+
+// @run-at document-end
+// ==/UserScript==
