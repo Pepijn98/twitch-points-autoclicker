8e585842909e296ba9372764b8f6560b8380541a
Pepijn98 <root@kurozeropb.info>
2020-09-19 15:57:07 +0200

    v1.2.0
    - Remove all es6 features since it gives weird behaviour

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Twitch.tv Channel Points Auto-clicker
 // @namespace       twitch-points-autoclicker
-// @version         1.1.0
+// @version         1.2.0
 // @description     Auto-click the green channel point button for you
 // @author          Pepijn98
 // @match           https://www.twitch.tv/*
@@ -12,8 +12,6 @@
 // @exclude         https://www.twitch.tv/directory/*
 // @grant           none
 
-// @require https://cdn.jsdelivr.net/npm/yukikaze@0.3.0/lib/index.min.js
-
 // @homepageURL https://github.com/Pepijn98/twitch-points-autoclicker
 // @supportURL https://github.com/Pepijn98/twitch-points-autoclicker/issues
 // @updateURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/master/index.meta.js
@@ -22,19 +20,67 @@
 // @run-at document-end
 // ==/UserScript==
 
-(() => {
-    const interval = new Yukikaze();
+/**
+ * Create a more accurate setInterval
+ */
+function Interval() {
+    this.active = false;
+    this.baseline = undefined;
+    this.timer = undefined;
+    this.first = true;
+}
+
+/**
+ * Start the interval
+ * 
+ * @param {(...args: any[]) => void} fn The function with all the stuff that needs to be done every interval
+ * @param {number} duration The duration in milliseconds in which each interval happens
+ * @param {boolean} initial Whether to imediatlly execute fn or wait the duration before executing fn for the first time
+ */
+Interval.prototype.run = function(fn, duration, initial = false) {
+    if (this.baseline === undefined) {
+        this.baseline = new Date().getTime();
+    }
+
+    if ((initial && this.first) || !this.first) fn();
+    if (this.first) this.first = false;
+
+    var end = new Date().getTime();
+    this.baseline += duration;
+
+    let nextTick = duration - (end - this.baseline);
+    if (nextTick < 0) {
+        nextTick = 0;
+    }
+
+    this.timer = setTimeout(() => this.run(fn, duration), nextTick);
+    this.active = true;
+}
+
+/**
+ * Stop the currently running interval if there is one
+ */
+Interval.prototype.stop = function() {
+    if (this.timer) {
+        clearTimeout(this.timer);
+        this.timer = undefined;
+        this.active = false;
+    }
+}
+
+(function() {
+    var interval = new Interval();
 
     // Check every 5 seconds if we can collect our points
     interval.run(() => {
         // Get all divs that are a direct child of community-points-summary
-        const divs = document.querySelectorAll(".community-points-summary > div");
+        var divs = document.querySelectorAll(".community-points-summary > div");
         // Get the div that has the button in it
-        const div = divs[1];
+        var div = divs[1];
         // Make sure it isn't undefined
         if (div) {
             // There should only be 1 button in this div so we can safely use [0] to get the first element
-            const btn = div.getElementsByTagName("button")[0];
+            var btn = div.getElementsByTagName("button")[0];
             // Check if button is available
             if (btn) {
                 // Emulate a click that collects our points :)
