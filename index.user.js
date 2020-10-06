38d72386ffbdb403058c568054bda6adff49d3da
Pepijn98 <root@kurozeropb.info>
2020-10-06 12:40:50 +0200

    Conver to es6

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Twitch.tv Channel Points Auto-clicker
 // @namespace       twitch-points-autoclicker
-// @version         1.2.1
+// @version         1.2.1-es6
 // @description     Auto-click the green channel point button for you
 // @author          Pepijn98
 // @match           https://www.twitch.tv/*
@@ -9,8 +9,8 @@
 
 // @homepageURL https://github.com/Pepijn98/twitch-points-autoclicker
 // @supportURL https://github.com/Pepijn98/twitch-points-autoclicker/issues
-// @updateURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/master/index.meta.js
-// @downloadURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/master/index.user.js
+// @updateURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/es6/index.meta.js
+// @downloadURL https://raw.githubusercontent.com/Pepijn98/twitch-points-autoclicker/es6/index.user.js
 
 // @run-at document-end
 // ==/UserScript==
@@ -18,13 +18,14 @@
 /**
  * Create a more accurate setInterval
  */
-function Interval() {
-    this.active = false;
-    this.baseline = undefined;
-    this.timer = undefined;
-    this.first = true;
-
-    var self = this;
+class Interval {
+    
+    constructor() {
+        this.active = false;
+        this.baseline = undefined;
+        this.timer = undefined;
+        this.first = true;
+    }
 
     /**
      * Start the interval
@@ -33,55 +34,55 @@ function Interval() {
      * @param {number} duration The duration in milliseconds in which each interval happens
      * @param {boolean} initial Whether to imediatlly execute fn or wait the duration before executing fn for the first time
      */
-    self.run = function(fn, duration, initial) {
+    run(fn, duration, initial) {
         if (!initial) initial = false;
 
-        if (self.baseline === undefined) {
-            self.baseline = new Date().getTime();
+        if (this.baseline === undefined) {
+            this.baseline = new Date().getTime();
         }
 
-        if ((initial && self.first) || !self.first) fn();
-        if (self.first) self.first = false;
+        if ((initial && this.first) || !this.first) fn();
+        if (this.first) this.first = false;
 
-        var end = new Date().getTime();
-        self.baseline += duration;
+        let end = new Date().getTime();
+        this.baseline += duration;
 
-        var nextTick = duration - (end - self.baseline);
+        let nextTick = duration - (end - this.baseline);
         if (nextTick < 0) {
             nextTick = 0;
         }
 
-        self.timer = setTimeout(function() {
-            self.run(fn, duration)
+        this.timer = setTimeout(() => {
+            this.run(fn, duration)
         }, nextTick);
-        self.active = true;
+        this.active = true;
     };
 
     /**
      * Stop the currently running interval if there is one
      */
-    self.stop = function() {
-        if (self.timer) {
-            clearTimeout(self.timer);
-            self.timer = undefined;
-            self.active = false;
+    stop() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = undefined;
+            this.active = false;
         }
     }
 };
 
-(function() {
-    var interval = new Interval();
+(() => {
+    let interval = new Interval();
 
     // Check every 5 seconds if we can collect our points
-    interval.run(function() {
+    interval.run(() => {
         // Get all divs that are a direct child of community-points-summary
-        var divs = document.querySelectorAll(".community-points-summary > div");
+        let divs = document.querySelectorAll(".community-points-summary > div");
         // Get the div that has the button in it
-        var div = divs[1];
+        let div = divs[1];
         // Make sure it isn't undefined
         if (div) {
             // There should only be 1 button in this div so we can safely use [0] to get the first element
-            var btn = div.getElementsByTagName("button")[0];
+            let btn = div.getElementsByTagName("button")[0];
             // Check if button is available
             if (btn) {
                 // Emulate a click that collects our points :)
